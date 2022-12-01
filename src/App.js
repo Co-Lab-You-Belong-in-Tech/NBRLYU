8775c37d33061d1903542568b8edb91c4f744d7b
ChristineWest <christinewwwest@gmail.com>
2022-12-01 12:29:50 -0500

    set useEffect to call firebase on page load

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import Home from "./pages/Home";
 import Request from "./pages/Request";
 import Provide from "./pages/Provide";
+import { getDatabase, ref, onValue, get } from "firebase/database";
 import firebase from "./firebase";
+import { useEffect } from "react";
 function App() {
+  useEffect(() => {
+    const database = getDatabase(firebase);
+    const dbRef = ref(database);
+    get(dbRef)
+      .then((snapshot) => {
+        if (snapshot.exists()) {
+          const data = snapshot.val();
+        }
+      })
+      .catch((error) => {
+        alert("There is no request matches your search. Please try again!");
+      });
+  }, []);
   return (
     <div>
       <header>NBRLYU</header>
