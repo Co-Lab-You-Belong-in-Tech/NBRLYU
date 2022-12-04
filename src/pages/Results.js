0234b7cdb5bb37796e3a6e02fe106b42a64cc9bb
ChristineWest <christinewwwest@gmail.com>
2022-12-04 15:32:49 -0500

    added Results page

---
+++ src/pages/Results.js
@@ -0,0 +1,12 @@
+function Results() {
+  return (
+    <div>
+      <h2>Here Are The Chores Match Your Search Criteria</h2>
+      <div>Empty Space for the Matched Forms from Firebase</div>
+    </div>
+  );
+}
+export default Results;
+
+//? Pseudo Code
+//? conditional rendering if there are forms matching providers search criteria: zip code and chore categories or error message if there is none matched
